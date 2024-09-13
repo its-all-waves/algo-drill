f5b6a5e0b6bc2723b3ec03b8947388cdaaf67b02
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2024-09-12 19:02:39 -0500

    add: key press TAB causes indent instead of loss of textbox focus

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 const BACKEND = 'http://localhost:8000'
@@ -11,11 +11,11 @@ export default function App() {
     )
 
     const [typedText, setTypedText] = useState('')
-    
+
     return (
         <>
             <div id="wrapper">
-                <TypingInputBackground 
+                <TypingInputBackground
                     content={algoText} 
                     setContent={setAlgoText}/>
                 <TypingInput 
@@ -26,16 +26,35 @@ export default function App() {
     )
 }
 
-
-
 function TypingInput({ value, setValue }) {
+    // const self = useRef(null)
+
+    useEffect(() => { 
+        
+    }, [])
+
+    function handleKeyDown(e) {
+        if (e.key != 'Tab') return
+        e.preventDefault()
+        const t = e.target
+        const start = t.selectionStart
+        const end = t.selectionEnd
+
+        const TAB = '    ' // tab appears as 8 spaces for some reason
+        
+        t.value = t.value.substring(0, start) + TAB + t.value.substring(end)
+        
+        t.selectionStart = t.selectionEnd = start + TAB.length
+    }
+    
     return <textarea
         id="typing-input"
         name="typing-input"
         cols="40"
         rows="20"
         value={value}
-        onChange={(e) => setValue(e.target.value)} />
+        onChange={(e) => setValue(e.target.value)} 
+        onKeyDown={handleKeyDown} />
 }
 
 function TypingInputBackground({ content, setContent }) {
