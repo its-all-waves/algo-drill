60dcb7c4fe2af9242aaaddf051569fcad8ac8216
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2024-09-20 11:48:39 -0500

    break out glass pane component from TextBox

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import TextBox from './App.jsx'
 import './index.css'
+
+// DEBUG
+import MainMenu from './components/MainMenu/MainMenu'
+import './App.css'
+import GlassPane from './components/GlassPane/GlassPane.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <App />
+    <TextBox />
+    // <MainMenu />
+    // <GlassPane />
   // </StrictMode>,
 )
